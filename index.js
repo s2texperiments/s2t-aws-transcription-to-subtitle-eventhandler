95d2aa8fae8a4288f75fc3b45001d0a042f72cd5
ste-xx <stefan.breitenstein@gmail.com>
2018-07-22 08:25:24 +0200

    [fetch result]

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
+const fetch = require("node-fetch");
+
 exports.handler = async (event) => {
 
     console.log(`Event: ${JSON.stringify(event)}`);
+    let {
+        Records: [{
+            Sns: {
+                Message: msg,
+                MessageAttributes: {
+                    "api-key-id": {
+                        Value: apiKeyId
+                    },
+                    "pid": {
+                        Value: pid
+                    },
+                }
+            }
+        }]
+    } = event;
 
+    let result = await fetch(JSON.parse(msg).TranscriptFileUri)
+        .then(response => response.json());
+
+    console.log(`Result: ${result}`);
 };
