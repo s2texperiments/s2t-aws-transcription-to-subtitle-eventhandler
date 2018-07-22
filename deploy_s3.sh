78b5baffeb202f8b2c05d815b20db6d7a5f245ce
ste-xx <stefan.breitenstein@gmail.com>
2018-07-22 12:34:24 +0200

    exec. permission for deploy script

--- deploy_s3.sh
+++ deploy_s3.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
 mkdir deploy
-zipName=TRAVIS_REPO_SLUG
-
-echo ${zipName}.zip
-zip -r deploy/s2t-completed-aws-transcription-message-to-subtitle-eventhandler.zip *.js node_modules/
+zip -r deploy/s2t-aws-transcription-to-subtitle-eventhandler.zip *.js node_modules/
